2e208b1217ce1e5471fa330cd44accd657c2fa11
Bimsara <44497039+BimsaraGunarathna@users.noreply.github.com>
2020-10-28 15:35:49 +0530

    Go to GPA btn is set and working

--- razor-gpa-web-app/Pages/Index.cshtml.cs
+++ razor-gpa-web-app/Pages/Index.cshtml.cs
@@ -1,25 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using razor_gpa_web_app.Areas.Identity.Data;
 
 namespace razor_gpa_web_app.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        UserManager<ApplicationUser> _userManager;
+        public SignInManager<ApplicationUser> _signInManager;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(
+            ILogger<IndexModel> logger, 
+            UserManager<ApplicationUser> userManager, 
+            SignInManager<ApplicationUser> signInManager
+            )
         {
             _logger = logger;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
-        public void OnGet()
-        {
+        public int UserRole { get; set; }
 
+        public async void OnGet()
+        {
+            //get the user id
+            //UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            //Get the user
+            //var AppUser = await _userManager.GetUserAsync(User);
+            //UserRole = AppUser.UserRole;
         }
     }
 }
